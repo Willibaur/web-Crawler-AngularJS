dc07bac11a2a6b608a89097b5f6392d5d16faba7
William Bautista <williambautista@gmail.com>
2016-05-10 16:33:39 +0100

    Karma config added

---
+++ test/karma.conf.js
@@ -0,0 +1,37 @@
+module.exports = function(config) {
+  config.set({
+
+    basePath: '',
+
+    frameworks: ['jasmine'],
+
+    files: [
+      'bower_components/angular/angular.js',
+      'bower_components/angular-route/angular-route.js',
+      'bower_components/angular-resource/angular-resource.js',
+      'bower_components/angular-mocks/angular-mocks.js',
+      'js/**/*.js',
+      'test/**/*.spec.js'
+    ],
+
+    exclude: [],
+
+    preprocessors: {},
+
+    reporters: ['verbose', 'progress'],
+
+    port: 9876,
+
+    colors: true,
+
+    logLevel: config.LOG_INFO,
+
+    autoWatch: true,
+
+    browsers: ['PhantomJS'],
+
+    singleRun: true,
+
+    concurrency: Infinity
+  });
+};
